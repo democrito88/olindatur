862f06d4c31dcaf210eb7ca5c46796dabfa33c56
LuizFernando1176 <nando1176@hotmail.com>
2024-11-06 14:11:21 -0300

    06/11

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,36 @@
 import "./Home.css";
-import dados from "../../assets/json/database.json";
 import Cabecalho from "../../components/Cabecalho";
 import useFetch from "../../hooks/useFetch";
 
 function Home() {
-    const {data, loading, mensagem} = useFetch("http://localhost:5173/json/database.json");
+    const { dados, estado, mensagem } = useFetch("http://localhost:5173/json/database.json");
 
     return (
         <>
+        
             <Cabecalho />
             <div className="container principal">
                 <div className="row d-flex align-items-center flex-wrap mt-2 mb-2">
-                    {loading ? <p>Carregando...</p> :
-                    Object.keys(data)?.map(propriedade =>
-                        <div key={propriedade} className="col-md-3 col-sm-3">
-                            <a href={`/${propriedade}`}>
-                                <div className="botao-categoria" style={{ backgroundImage: `url(${dados[propriedade][0].url})` }}>
-                                    <div className="botao-categoria-painel">
-                                        <h4 className="botao-categoria-titulo">
-                                            {propriedade}
-                                        </h4>
+                    {/* Se estiver carregando, exibe "Carregando..." */}
+                    {estado == 0 || estado == 1 ? (
+                        <p>{mensagem}</p>
+                    ) : (
+                        // Se os dados estiverem carregados e não houver erro, renderiza os botões de categoria
+                       
+                        Object.keys(dados)?.map(estado => (
+                            
+                            <div key={estado} className="col-md-3 col-sm-3">
+                                <a href={`/${estado}`}>
+                                    <div className="botao-categoria" style={{ backgroundImage: `url(${dados[estado][0].url})` }}>
+                                        <div className="botao-categoria-painel">
+                                            <h4 className="botao-categoria-titulo">
+                                                {estado}
+                                            </h4>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
+                                </a>
+                            </div>
+                        ))
                     )}
                 </div>
             </div>
