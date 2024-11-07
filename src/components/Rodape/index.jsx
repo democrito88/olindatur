abc5dcb3077422e7b7896ab24f960761605b5890
LuizFernando1176 <nando1176@hotmail.com>
2024-11-07 13:50:51 -0300

    07/11

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import "./Rodape.css"
+import { IoLogoYoutube } from "react-icons/io";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { SiInstagram } from "react-icons/si";
 
-function Rodape(){
+function Rodape() {
     return (
         <footer className="rodape mt-2">
-            <h6>Olindatur &copy;</h6>
-            <p>
-                Prefeitura Municipal de Olinda - Todos  os direitos reservados
-            </p>
+            <div class="container">
+                <div class="row mt-3 d-flex flex-row align-itens-baseline" >
+                    <div class="col-sm">
+                        <img src='../public/brasao.png' width={'200px'} className='left' />
+                    </div>
+                    <div class="col-sm ">
+                        <img src="../public/fSemFundo.png" width="50px" alt="Logo Olindatur" /><b>Olindatur &copy;</b>
+                    </div>
+                    <div class="col-sm link-social">
+                        <a href="#">
+                            <IoLogoYoutube size={'35px'} />
+                        </a>
+                        <a href="#">
+                            <FaFacebookSquare size={'35px'} />
+                        </a>
+                        <a href="#">
+                            <FaXTwitter size={'35px'} />
+                        </a>
+                        <a href="#">
+                            <SiInstagram size={'35px'} />
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            <p><b>Prefeitura Municipal de Olinda - Todos os direitos reservados.</b></p>
         </footer>
     );
 }
