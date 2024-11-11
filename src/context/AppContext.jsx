d9ae9c9d6d789135da2c6fcf1a22b5e861deb919
democrito88 <d_who@hotmail.com>
2024-11-11 13:09:26 -0300

    11-11-2024

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,7 +4,6 @@ import useFetch from '../hooks/useFetch';
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const urlBase = "http://localhost:5173/olindatur/";
   const {dados, estado, mensagem} = useFetch(`../src/assets/json/database.json`);
   
   return (
