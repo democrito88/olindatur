7012a58aa0e23927d508f3a209e1003156a3b6b0
LuizFernando1176 <nando1176@hotmail.com>
2024-11-12 10:49:10 -0300

    12/11

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  // base: '/olindatur/',
-=======
   //base: '/olindatur/',
->>>>>>> e7cf1c43688a1ed03907531bb136c85efab5e739
   plugins: [react()],
   resolve: {
     alias: {
