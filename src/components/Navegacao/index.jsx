61e896e6720228bd8c070f113ab44618343d235c
democrito88 <d_who@hotmail.com>
2024-11-13 13:50:13 -0300

    13-11-2024

--- src/components/Navegacao/index.jsx
+++ src/components/Navegacao/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { AppContext } from "../../context/AppContext";
 
@@ -13,12 +13,12 @@ export default function Navegacao() {
     <Navbar expand="lg" className="bg-body-tertiary capitalize">
       <Container>
         <Navbar.Brand href="/" className="d-flex flex-row align-items-baseline">
-          <img src="/favicon.png" width={20} />
+          <img src="/favicon.png" width={20} alt="Logo" />
           OlindaTur
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto d-flex flex-row justify-content-end">
             <NavDropdown title={t('dropdown-menu')} id="basic-nav-dropdown">
               {estado === 0 || estado === 1 ? (
                 <p>{mensagem}</p>
@@ -31,21 +31,29 @@ export default function Navegacao() {
               )}
             </NavDropdown>
           </Nav>
+          <Nav className="ms-auto d-flex flex-row">
+            <Nav.Item>
+              <Nav.Link onClick={() => changeLanguage('pt')} style={{ fontSize: '35px' }}>
+                🇧🇷
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={() => changeLanguage('en')} style={{ fontSize: '35px' }}>
+                🏴󠁧󠁢󠁥󠁮󠁧󠁿
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={() => changeLanguage('es')} style={{ fontSize: '35px' }}>
+                🇪🇸
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={() => changeLanguage('cn')} style={{ fontSize: '35px' }}>
+                🇨🇳
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
         </Navbar.Collapse>
-        <Nav>
-          <Nav.Link onClick={() => changeLanguage('pt')} style={{ fontSize: '35px' }}>
-            🇧🇷
-          </Nav.Link>
-          <Nav.Link onClick={() => changeLanguage('en')} style={{ fontSize: '35px' }}>
-            🏴󠁧󠁢󠁥󠁮󠁧󠁿
-          </Nav.Link>
-          <Nav.Link onClick={() => changeLanguage('es')} style={{ fontSize: '35px' }}>
-            🇪🇸
-          </Nav.Link>
-          <Nav.Link onClick={() => changeLanguage('cn')} style={{ fontSize: '35px' }}>
-            🇨🇳
-          </Nav.Link>
-        </Nav>
       </Container>
     </Navbar>
   );
