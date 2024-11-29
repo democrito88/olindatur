581fda322f09f3640daee5bd67a5b93d43076e6a
democrito88 <d_who@hotmail.com>
2024-11-29 15:26:35 -0300

    29-11-2024

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //base: '/',
+  base: '/olindatur/',
   plugins: [react()],
   /*/Adicionar isto para funcionar no docker
   server: {
